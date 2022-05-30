9a7d43cf566a8323a0e8a98b330d20bf14947788
malindalakshan <malinda.lakshan2012@gmail.com>
2022-05-30 22:48:39 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using CSharpFundamentals.Math;
+using System;
+
+namespace CSharpFundamentals
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person malinda = new person();
+            malinda.FullName = "Malinda";
+            malinda.LastName = "Lakshan";
+            malinda.Introduce();
+
+            Calculater calculater = new Calculater();
+            var answer=calculater.Add(1, 2);
+            Console.WriteLine(answer);
+            
+
+        }
+    }
+}
---
+++ person.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpFundamentals
+{
+    public class person{
+        public String FullName;
+        public String LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My full name is"+" " + FullName + " " + LastName);
+        }
+    }
+}
